31a2a03399dbf57687b1b18703e78efa217fbc34
TatVSam <ts_27@bk.ru>
2022-12-04 21:08:44 +0300

    init

---
+++ application/views/template_view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html> 
+<html lang="ru"> 
+
+    <head> 
+    <meta charset="utf-8"> 
+    <title>Визитка</title> 
+</head> 
+
+<body>
+<h3>Ссылки</h3>
+<ul>
+    <li><a href="/">Главная</a></li>
+    <li><a href="/about">О нас</a></li>
+    <li><a href="/contacts">Контакты</a></li>
+    <li><a href="/services">Услуги</a></li>
+</ul>
+Эту надпись видно на всех страницах.
+    <?php include $content_view; ?>
+</body> 
+
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+ini_set('display_errors', 1);
+//var_dump($_GET);
+//echo $_SERVER['REQUEST_URI'];
+
+require_once 'application/bootstrap.php';
+
+
+
+?>
